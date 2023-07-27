b7b0098a5058738ae7c481b5675cdcd483f61e08
Gilbert Langat <Gilbert.Langat@dentsu.com>
2023-07-27 14:59:47 +0300

    Disable Client-Side Caching

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 var ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
