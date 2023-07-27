7150198ad235be332d513a6d2958825a9c31da82
Gilbert Langat <Gilbert.Langat@dentsu.com>
2023-07-27 15:05:24 +0300

    added Content Security Policy

--- myApp.js
+++ myApp.js
@@ -9,6 +9,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ));
 
 var ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
